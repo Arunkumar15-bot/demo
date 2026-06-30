b566f3070dd215ac817920bf9f43daac8e9affea
ARUNKUMAR <76714089+Arunkumar15-bot@users.noreply.github.com>
2026-06-30 18:22:47 +0530

    Add files via upload

---
+++ tests/saucedemo.spec.js
@@ -0,0 +1,20 @@
+const { test,expect } = require('@playwright/test');
+const LoginPage = require('../pages/loginpage');
+const ProductPage = require('../pages/productpage');
+const CheckoutPage = require('../pages/checkoutpage');
+
+test('SauceDemo Test', async ({ page }) => {
+
+    const loginpage = new LoginPage(page);
+    const productpage = new ProductPage(page);
+    const checkoutpage = new CheckoutPage(page);
+
+    await loginpage.navigate();
+
+    await loginpage.login('standard_user', 'secret_sauce');
+
+    await productpage.addproduct();
+    await productpage.gotocart();
+
+    await checkoutpage.checkout('Arun', 'Kumar', '627758');
+});
